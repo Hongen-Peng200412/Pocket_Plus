bc5c6db055a545d2b2c3a528353f63003dfd1143
Hongen Peng <1591999801@qq.com>
2026-08-17 18:42:40 +0800

    删除退出主线的 Stage1 历史实现

--- utils/validate_density_map_pairs.py
+++ utils/validate_density_map_pairs.py
@@ -14,7 +14,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from processedPDB_EMDB_binder.utils.mrc_tools import load_map
+from utils.mrc_tools import load_map
 
 DEFAULT_SIM_MAP_KEYS = ("sim_map_path", "sim_map_path_cryoatom")
 
